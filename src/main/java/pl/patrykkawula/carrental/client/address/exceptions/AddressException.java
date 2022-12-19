89f07c73bfd5696fe9985979d12416f275bd48a8
Patryk Kawula <kawul1996@gmail.com>
2022-12-19 18:32:59 +0100

    Create exception AddressException in address.exceptions package

---
+++ src/main/java/pl/patrykkawula/carrental/client/address/exceptions/AddressException.java
@@ -0,0 +1,15 @@
+package pl.patrykkawula.carrental.client.address.exceptions;
+
+public class AddressException extends RuntimeException {
+    public final Type type;
+
+    public AddressException(Long id, final Type type) {
+        super("Address with id: " + id);
+        this.type = type;
+    }
+
+    public enum Type {
+        NOT_FOUND,
+        ALREADY_EXISTS
+    }
+}
