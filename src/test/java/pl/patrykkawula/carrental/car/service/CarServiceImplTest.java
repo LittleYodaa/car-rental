ae90011a92c5ccc2875d55de6426955599944739
Patryk Kawula <kawul1996@gmail.com>
2022-12-26 15:09:08 +0100

    Create CarServiceImplTest

---
+++ src/test/java/pl/patrykkawula/carrental/car/service/CarServiceImplTest.java
@@ -0,0 +1,33 @@
+package pl.patrykkawula.carrental.car.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
