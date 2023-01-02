cb26f6b4992c9947b965e8b63317f944cfc665d7
Patryk Kawula <kawul1996@gmail.com>
2023-01-02 19:00:39 +0100

    Create CarServiceImplTest

--- src/test/java/pl/patrykkawula/carrental/car/service/CarServiceImplTest.java
+++ src/test/java/pl/patrykkawula/carrental/car/service/CarServiceImplTest.java
@@ -15,51 +15,94 @@ import pl.patrykkawula.carrental.car.model.EngineType;
 import pl.patrykkawula.carrental.car.model.Gearbox;
 
 import java.time.Year;
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CarServiceImplTest {
 
-  @Autowired
-  CarService underTest;
+    @Autowired
+    CarService underTest;
 
-  @Autowired
-  CarRepository carRepository;
+    @Autowired
+    CarRepository carRepository;
 
-  @AfterEach
-  void afterEach() {
-    carRepository.deleteAll();
-  }
+    @AfterEach
+    void afterEach() {
+        carRepository.deleteAll();
+    }
 
-  @Test
-  void shouldSaveNewCar() {
+    @Test
+    void shouldSaveNewCar() {
 //        given:
-    CarDto carToSave = new CarDto("testBrand", "testModel", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
-        Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        CarDto carToSave = new CarDto("testBrand", "testModel", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
 
 //        when:
-    final CarDto savedCar = underTest.save(carToSave);
+        final CarDto savedCar = underTest.save(carToSave);
 
 //        then:
-    //  czy w zapisanym aucie zgadza się liczba miejsc
-    Assertions.assertEquals(5, carRepository.findAll().get(0).getSeats());
-    //  czy w zwracanym dto zgadza się liczba miejsc
-    Assertions.assertEquals(5, savedCar.seats());
-    //  czy w repo jest jedno auto
-    Assertions.assertEquals(1, carRepository.findAll().size());
-  }
+        Assertions.assertEquals(5, carRepository.findAll().get(0).getSeats());
+        Assertions.assertEquals(5, savedCar.seats());
+        Assertions.assertEquals(1, carRepository.findAll().size());
+    }
 
-  @Test
-  void shouldDeleteCar() {
-    //        given:
-    Car carToSave = new Car("testBrand", "testModel", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
-        Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
-    carRepository.save(carToSave);
+    void shouldUpdateCar1ToCar2() {
+        //given
+        CarDto carToSave1 = new CarDto("testBrand1", "testModel1", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        CarDto carToSave2 = new CarDto("testBrand2", "testModel2", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        carRepository.save(carToSave1);
+        carRepository.save(carToSave2);
 
-//        when:
-    underTest.delete(1L);
+        //when
+        underTest.update(carToSave1.getId(), carToSave2)
+    }
 
-//    then:
-    Assertions.assertEquals(0, carRepository.findAll().size());
-  }
+    @Test
+    void shouldDeleteCar() {
+        //given
+        Car carToSave = new Car("testBrand", "testModel", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        carRepository.save(carToSave);
+
+        //when
+        underTest.delete(1L);
+
+        //then
+        Assertions.assertEquals(0, carRepository.findAll().size());
+    }
+    @Test
+    void shouldReturnCar() {
+        //given
+        Car carToSave = new Car("testBrand", "testModel", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        carRepository.save(carToSave);
+
+        //when
+        CarDto returnCar = underTest.get(carToSave.getId());
+
+        //then
+        Assertions.assertEquals("12345", returnCar.vin());
+        Assertions.assertEquals(5, returnCar.seats());
+    }
+
+    @Test
+    void shouldReturnListOfTwoCars() {
+        //given
+        Car carToSave1 = new Car("testBrand1", "testModel1", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        Car carToSave2 = new Car("testBrand2", "testModel2", new Engine(150, 100, EngineType.PETROL), Gearbox.AUTOMATIC, 100.00, "12345",
+                Year.of(2010), CarType.LIMOUSINE, CarSegment.D, 5);
+        carRepository.save(carToSave1);
+        carRepository.save(carToSave2);
+
+        //when
+        List<CarDto> carList = underTest.getAll();
+
+        //then
+        Assertions.assertEquals(carRepository.findAll().size(), carList.size());
+        Assertions.assertEquals("testModel2", carToSave2.getModel());
+    }
 }
